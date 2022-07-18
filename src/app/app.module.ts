4bc9f9d8709f2e58bf44ebb4c9662facf7452caa
michal <abc@lol.com>
2022-07-18 14:59:44 +0100

    added json server and delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./components/header/header.component";
@@ -19,6 +20,7 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
   imports: [
     BrowserModule,
     FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
